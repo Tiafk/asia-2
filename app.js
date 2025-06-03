5e656d1940b4b03367fbdc597c3aa360a127c6eb
Tiafk <akchurin227@mail.ru>
2025-06-03 18:29:10 +0300

    asia-video

---
+++ app.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const swiper = new Swiper('.swiper', {
+    loop: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+        clickable: true,
+    },
+  });
+});
